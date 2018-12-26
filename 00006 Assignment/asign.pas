3db5a3a64bb205dc6b6fb7e5f4b4692ca22503f6
Brama Hendra Mahendra <bramahendramahendra@gmail.com>
2018-12-27 05:19:10 +0700

    Add files via upload

---
+++ 00006 Assignment/asign.pas
@@ -0,0 +1,27 @@
+program asign;
+
+uses crt;
+
+var
+        x,y:integer;
+        z:string;
+
+BEGIN
+        clrscr;
+
+        writeln('contoh program assignment');
+        //-----input data----
+        x:=100;
+        y:=500;
+        z:='brama';
+        //------output data--------
+        writeln(' Nilai x : ',x);
+        writeln(' Nilai y : ',y);
+        writeln(' Nilai z : ',z);
+        //---output maxint dan maxlongint----
+        writeln(' Nilai max integer : ',maxint);
+        writeln(' Nilai max longint : ',maxlongint);
+
+        readln;
+END.
+
